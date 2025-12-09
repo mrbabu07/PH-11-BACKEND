be640288fe2cf58fbd667f48316ebacad6dc844e
md jahedul islam <mdjahedulislamjaved@gmail.com>
2025-12-09 12:19:06 +0600

    added the server

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res)=>{
+res.send('Hello World!');
+
+});
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
